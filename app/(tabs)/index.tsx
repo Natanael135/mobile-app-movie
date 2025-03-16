6f266f761e0b7429aac12b982ab21da8b088752f
Natanael Melo <109748470+Natanael135@users.noreply.github.com>
2025-03-16 10:37:46 -0300

    feat: implementando ajustes na pagina inicial.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,33 @@
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Text, View, Image, ScrollView } from "react-native";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
   return (
-    <View className="flex-1 items-center justify-center ">
-      <Text className="text-5xl text-dark-200 font-bold">Bem vindo</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+
+      <ScrollView
+        className="flex-1 px-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          paddingBottom: 10,
+        }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        <View className="flex-1 mt-5">
+          <SearchBar
+          onPress={() => router.push("/search")}	
+          pllaceholder="Search"
+          
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }

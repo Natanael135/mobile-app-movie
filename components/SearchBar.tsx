ad996df71d0e2e435574667144a2419c000706f1
Natanael Melo <109748470+Natanael135@users.noreply.github.com>
2025-03-16 13:55:37 -0300

    fix: corrigindo bug na barra de pesquisa.

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,28 +1,30 @@
-import { View, Text, Image, TextInput } from "react-native";
-import React from "react";
+import { View, TextInput, Image } from "react-native";
+
 import { icons } from "@/constants/icons";
 
-interface props {
+interface Props {
   placeholder: string;
+  value?: string;
+  onChangeText?: (text: string) => void;
   onPress?: () => void;
 }
 
-const SearchBar = ({ placeholder, onPress }: props) => {
+const SearchBar = ({ placeholder, value, onChangeText, onPress }: Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image
         source={icons.search}
-        className="size-5"
+        className="w-5 h-5"
         resizeMode="contain"
-        tintColor={"#ab8bff"}
+        tintColor="#AB8BFF"
       />
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChangeText={() => {}}
-        placeholderTextColor={"#a8b5db"}
+        value={value}
+        onChangeText={onChangeText}
         className="flex-1 ml-2 text-white"
+        placeholderTextColor="#A8B5DB"
       />
     </View>
   );
